b5800b639a462b44600ae01081469e3b08972134
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-04-26 21:48:45 +0200

    Connected to my MongoDB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,15 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import pingRoutes from "./routes/ping.routes";
+import { connectDB } from "./config/db";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// MongoDB connection
+connectDB();
 
 app.use(cors());
 app.use(express.json());
