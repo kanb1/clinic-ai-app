d19dd55e26e42945a0f7fec33df46cf814cb4dee
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-04-27 12:48:27 +0200

    Mongoose model for user

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  role: "patient" | "doctor" | "secretary" | "admin";
+  clinic_id: mongoose.Types.ObjectId;
+  email: string;
+  password_hash: string;
+  phone: string;
+  address: string;
+  birth_date: Date;
+  cpr_number: string;
+  status: "ledig" | "optaget" | "ferie" | "syg" | "andet";
+}
+
+const UserSchema: Schema = new Schema(
+  {
+    name: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["patient", "doctor", "secretary", "admin"],
+      required: true,
+    },
+    clinic_id: { type: mongoose.Schema.Types.ObjectId, ref: "Clinic" },
+    email: { type: String, required: true, unique: true },
+    password_hash: { type: String, required: true },
+    phone: { type: String },
+    address: { type: String },
+    birth_date: { type: Date },
+    cpr_number: { type: String },
+    status: {
+      type: String,
+      enum: ["ledig", "optaget", "ferie", "syg", "andet"],
+      default: "ledig",
+    },
+  },
+  { timestamps: true }
+);
+
+export const UserModel = mongoose.model<IUser>("User", UserSchema);
