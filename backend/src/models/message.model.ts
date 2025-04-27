dbc68e37dcf2703452cd23d011c3ae473d6cc49a
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-04-27 13:10:36 +0200

    Mongoose model for message

---
+++ backend/src/models/message.model.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IMessage extends Document {
+  sender_id: mongoose.Types.ObjectId;
+  receiver_id: mongoose.Types.ObjectId;
+  content: string;
+  read: boolean;
+}
+
+const MessageSchema: Schema = new Schema(
+  {
+    sender_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: { type: String, required: true },
+    read: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export const MessageModel = mongoose.model<IMessage>("Message", MessageSchema);
