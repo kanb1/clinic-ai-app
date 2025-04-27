183cce10ac79acfdf481ced95c47f3e66d218b50
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-04-27 13:07:21 +0200

    Mongoose model for appointments

---
+++ backend/src/models/appointment.model.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IAppointment extends Document {
+  patient_id: mongoose.Types.ObjectId;
+  doctor_id: mongoose.Types.ObjectId;
+  clinic_id: mongoose.Types.ObjectId;
+  date: Date;
+  time: string;
+  status: "bekræftet" | "aflyst" | "udført" | "venter";
+  secretary_note?: string;
+}
+
+const AppointmentSchema: Schema = new Schema(
+  {
+    patient_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    doctor_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    clinic_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Clinic",
+      required: true,
+    },
+    date: { type: Date, required: true },
+    time: { type: String, required: true }, // "14:30"
+    status: {
+      type: String,
+      enum: ["bekræftet", "aflyst", "udført", "venter"],
+      default: "venter",
+      required: true,
+    },
+    secretary_note: { type: String },
+  },
+  { timestamps: true }
+);
+
+export const AppointmentModel = mongoose.model<IAppointment>(
+  "Appointment",
+  AppointmentSchema
+);
