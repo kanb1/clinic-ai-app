497e52b1e26c1233942665a506d7492e1232a655
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-04-27 12:53:36 +0200

    Mongoose model for Clinic

---
+++ backend/src/models/clinic.model.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IClinic extends Document {
+  name: string;
+  address: string;
+  created_by: mongoose.Types.ObjectId; //refererer til en user (admin eller doctor)
+}
+
+const ClinicSchema: Schema = new Schema(
+  {
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    created_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const ClinicModel = mongoose.model<IClinic>("Clinic", ClinicSchema);
