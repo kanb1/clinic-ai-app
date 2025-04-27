4a07f334e7d591003d4f05704af66150d741f8d1
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-04-27 13:11:00 +0200

    Mongoose model for testresults

---
+++ backend/src/models/testresult.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface ITestResult extends Document {
+  patient_id: mongoose.Types.ObjectId;
+  test_name: string;
+  result: string;
+  date: Date;
+}
+
+const TestResultSchema: Schema = new Schema(
+  {
+    patient_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    test_name: { type: String, required: true },
+    result: { type: String, required: true },
+    date: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
+
+export const TestResultModel = mongoose.model<ITestResult>(
+  "TestResult",
+  TestResultSchema
+);
