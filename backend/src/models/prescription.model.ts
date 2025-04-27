004f9babcc9f89baf9faac810ed69a7a25939c59
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-04-27 13:10:50 +0200

    Mongoose model for prescription

---
+++ backend/src/models/prescription.model.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IPrescription extends Document {
+  patient_id: mongoose.Types.ObjectId;
+  medication_name: string;
+  dosage: string;
+  instructions: string;
+  issued_date: Date;
+}
+
+const PrescriptionSchema: Schema = new Schema(
+  {
+    patient_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    medication_name: { type: String, required: true },
+    dosage: { type: String, required: true },
+    instructions: { type: String, required: true },
+    issued_date: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
+
+export const PrescriptionModel = mongoose.model<IPrescription>(
+  "Prescription",
+  PrescriptionSchema
+);
