42ed71424441ef1da277bd6be353493355cf9c0b
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-04-27 13:10:00 +0200

    Mongoose model for "Journal"

---
+++ backend/src/models/journal.model.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IJournal extends Document {
+  patient_id: mongoose.Types.ObjectId;
+  entries: mongoose.Types.ObjectId[]; // References to journal entries
+}
+
+const JournalSchema: Schema = new Schema(
+  {
+    patient_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    entries: [{ type: mongoose.Schema.Types.ObjectId, ref: "JournalEntry" }],
+  },
+  { timestamps: true }
+);
+
+export const JournalModel = mongoose.model<IJournal>("Journal", JournalSchema);
