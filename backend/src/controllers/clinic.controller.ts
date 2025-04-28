bef20320c84b8172f90f83363426586d25cb7359
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-04-28 13:50:06 +0200

    created the clinic controller

---
+++ backend/src/controllers/clinic.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { ClinicModel } from "../models/clinic.model";
+
+export const createClinic = async (req: Request, res: Response) => {
+  try {
+    const { name, address } = req.body;
+    const clinic = await ClinicModel.create({ name, address });
+    res.status(201).json(clinic);
+  } catch (error) {
+    res.status(500).json({ message: "Error when creating clinic", error });
+  }
+};
+
+export const getClinics = async (req: Request, res: Response) => {
+  try {
+    const clinics = await ClinicModel.find();
+    res.json(clinics);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching the clinics", error });
+  }
+};
