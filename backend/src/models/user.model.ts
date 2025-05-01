62d2a3faf5369b890bbd5d72bbe88838045383d6
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-05-01 15:00:23 +0200

    Implementeret pre-save middleware ift ændring af passwords

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcryptjs";
 
 export interface IUser extends Document {
   name: string;
@@ -36,5 +37,27 @@ const UserSchema: Schema = new Schema(
   },
   { timestamps: true }
 );
+
+// **********PRE-SAVE MIDDLEWARE
+// pre-save --> Kør denne funktion inden .save()
+// this: IUser betyder this refererer til et dokument som følger IUser interfacet. TS ting.. så ts ved this.password_hash det en string
+UserSchema.pre("save", async function (this: IUser, next) {
+  // Kun hvis password_hash er blevet ændret, så hasher vi noget
+  // return next: hopper videre til næste middleware/save
+  if (!this.isModified("password_hash")) return next();
+
+  try {
+    // genererer et salt med 10 runder, tilfældig værdi der tilføjes
+    const salt = await bcrypt.genSalt(10);
+    // hasher det rå password og overskriver feltet password_hash med den hash
+    // så når dokumentet gemmes bagefter er pass allerede krypteret
+    this.password_hash = await bcrypt.hash(this.password_hash, salt);
+    // jeg er færdig gå videre med at gemme dokumentet
+    next();
+  } catch (error) {
+    // hvis fejl, så sender vi fejlen videre til express- error handler
+    next(error as Error);
+  }
+});
 
 export const UserModel = mongoose.model<IUser>("User", UserSchema);
