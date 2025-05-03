52a4c8724b5b7b867fca6a80e95cd2758f96debb
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-05-03 19:23:17 +0200

    test: Fix testsexample

--- backend/src/tests/testexample.test.ts
+++ backend/src/tests/testexample.test.ts
@@ -1,6 +1,6 @@
 // this is just a test for CI
-describe("Dummy test", () => {
-  it("should work", () => {
+describe('Dummy test', () => {
+  it('should work', () => {
     expect(true).toBe(true);
   });
 });
