0ce6be626ca5a0b2cf98eee98df216057733f074
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-05-03 19:20:53 +0200

    test: Add minimum requirement of 75% for the Code Coverage

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -5,8 +5,16 @@ module.exports = {
   collectCoverage: true, //lav automatisk en mappekaldet collect inde i backend
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov"], //text viser rapport i terminalen som jeg ville have, lcov html rapport i coverage/lcov-report/index.html
-  testMatch: ["**/__tests__/**/*.test.ts"], //finder kun test-filer som ligger i en mappe kaldet __tests__ og slutter på .test.ts
+  testMatch: ["**/tests/**/*.test.ts"], //finder kun test-filer som ligger i en mappe kaldet __tests__ og slutter på .test.ts
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
+  },
+  coverageThreshold: {
+    global: {
+      branches: 75, //I coeverageTreshold angiver jeg minimumskrav på 75%
+      functions: 75,
+      lines: 75,
+      statements: 75,
+    },
   },
 };
--- backend/src/tests/testexample.test.ts
+++ backend/src/tests/testexample.test.ts
@@ -1,1 +1,6 @@
 // this is just a test for CI
+describe("Dummy test", () => {
+  it("should work", () => {
+    expect(true).toBe(true);
+  });
+});
