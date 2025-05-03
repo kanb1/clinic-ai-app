e204ad5d8b3222f0bf2fca445f7cab447fbece31
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-05-03 18:05:57 +0200

    fix(ci): correct coverage path in artifact upload

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: "ts-jest", //skriv test i ts
   testEnvironment: "node",
-  collectCoverage: true,
+  collectCoverage: true, //lav automatisk en mappekaldet collect inde i backend
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov"], //text viser rapport i terminalen som jeg ville have, lcov html rapport i coverage/lcov-report/index.html
   testMatch: ["**/__tests__/**/*.test.ts"], //finder kun test-filer som ligger i en mappe kaldet __tests__ og slutter på .test.ts
