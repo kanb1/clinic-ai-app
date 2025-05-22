3bfb667340f7c376a723bd9927ac6ae778c542a3
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-05-22 20:06:34 +0200

    style: Added responsive style to the page

--- frontend/src/pages/Frontpage/ChooseRolePage.tsx
+++ frontend/src/pages/Frontpage/ChooseRolePage.tsx
@@ -1,24 +1,30 @@
-import { Box, Heading, VStack, Button } from "@chakra-ui/react";
+import { Box, Heading, VStack, Button, Text } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 const ChooseRolePage = () => {
   const navigate = useNavigate();
 
   return (
-    <Box textAlign="center">
+    <Box textAlign="center" px={{ sm: 10, md: 0 }}>
       <Heading
         fontWeight="extrabold"
         fontFamily="heading"
         textStyle="heading1"
         mb={0}
+        mt={10}
       >
-        Velkommen
+        Velkommen til Klinika
       </Heading>
-      <Heading textStyle="body" fontWeight="thin" fontFamily="heading" mt={0}>
+      <Text
+        textStyle="body"
+        fontWeight="medium"
+        fontFamily="heading"
+        mt={{ lg: 3 }}
+      >
         Er du patient eller arbejder hos en klinik? Vælg din adgang herunder.
-      </Heading>
+      </Text>
 
-      <VStack gap={14} pt={20}>
+      <VStack gap={5} pt={{ sm: 6 }}>
         <Button
           variant="solidRed"
           minW="200px"
