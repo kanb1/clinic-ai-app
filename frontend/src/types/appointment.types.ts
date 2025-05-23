beb6da62398a9ce5cdcf73d11104ef8f34d9faf6
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-05-23 18:08:23 +0200

    refactor(appointment-model): Added end_time

---
+++ frontend/src/types/appointment.types.ts
@@ -0,0 +1,14 @@
+export interface IPersonMini {
+  _id: string;
+  name: string;
+}
+
+export interface IAppointment {
+  _id: string;
+  patient_id: IPersonMini;
+  doctor_id: IPersonMini;
+  date: string; // ISO timestamp
+  time: string; // fx "10:30"
+  end_time: string;
+  status: "bekræftet" | "aflyst" | "udført" | "venter";
+}
