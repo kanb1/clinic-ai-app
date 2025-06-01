6f74019dfe9e23214b35f236cc79a1e9d67efa7b
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-06-01 21:25:11 +0200

    feat: hook for fetching prescriptions

---
+++ frontend/src/hooks/doctor/journalHooks/usePrescriptions.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { api } from "@/services/httpClient";
+
+export const usePrescriptions = (patientId: string | null) => {
+  return useQuery({
+    queryKey: ["prescriptions", patientId],
+    queryFn: async () => {
+      if (!patientId) return [];
+      const res = await api.get(`/doctors/prescriptions/${patientId}`);
+      return res.data;
+    },
+    enabled: !!patientId,
+  });
+};
