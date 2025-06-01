ef0adc678d44da50f3b353ed19675ceaee2d816f
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-06-01 22:13:21 +0200

    feat: Created button navigation on patient frontpage

--- frontend/src/pages/Patient/PatientFrontpage.tsx
+++ frontend/src/pages/Patient/PatientFrontpage.tsx
@@ -1,12 +1,53 @@
 import Layout from "@/components/layout/Layout";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Link, Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 const PatientFrontpage = () => {
+  const navigate = useNavigate();
+
   return (
     <Layout>
-      <Box p={10}>
-        <Heading size="lg">Velkommen Patient</Heading>
-      </Box>
+      <Flex
+        direction="column"
+        alignItems="center"
+        w="full"
+        maxW={{ base: "100%" }}
+        mx="auto"
+      >
+        <Box
+          w="full"
+          maxW={{ base: "100%" }}
+          textAlign="center"
+          mt={{ base: 3 }}
+        >
+          <Heading size="heading1">Velkommen Patient </Heading>
+          <Text size="body">Vælg en af nedenstående</Text>
+          <Flex
+            direction="column"
+            alignItems="center"
+            maxW={{ base: "100%" }}
+            gap={{ base: 3 }}
+            mt={{ base: 5 }}
+          >
+            <Button
+              as={Link}
+              onClick={() => navigate("/patient/ai")}
+              variant="solidRed"
+              w={{ base: "15rem", sm: "17rem", md: "18rem", xl: "20rem" }}
+            >
+              Snak med Klinika Assistent
+            </Button>
+            <Button
+              as={Link}
+              onClick={() => navigate("/patient/appointments")}
+              variant="solidRed"
+              w={{ base: "15rem", sm: "17rem", md: "18rem", xl: "20rem" }}
+            >
+              Gå til Min Side
+            </Button>
+          </Flex>
+        </Box>
+      </Flex>
     </Layout>
   );
 };
