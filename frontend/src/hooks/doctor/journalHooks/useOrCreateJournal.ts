918f4b40f9a368f795897fbe81a2be861acb62c8
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-06-01 18:38:32 +0200

    feat: created hook to use or create journal when accessing patient's journalpage

---
+++ frontend/src/hooks/doctor/journalHooks/useOrCreateJournal.ts
@@ -0,0 +1,12 @@
+import { api } from "@/services/httpClient";
+import { useQuery } from "@tanstack/react-query";
+
+export const useOrCreateJournal = (patientId: string) => {
+  return useQuery({
+    queryKey: ["journalId", patientId],
+    queryFn: async () => {
+      const res = await api.get(`/doctors/journals/patient/${patientId}`);
+      return res.data; // indeholder journalId, entryCount, etc
+    },
+  });
+};
