49a84e13dce251c00972f6dbebcfad7f9937f6de
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-06-02 15:15:57 +0200

    style: Responsive style

--- frontend/src/pages/Patient/PatientFrontpage.tsx
+++ frontend/src/pages/Patient/PatientFrontpage.tsx
@@ -1,9 +1,11 @@
 import Layout from "@/components/layout/Layout";
+import { useAuth } from "@/context/AuthContext";
 import { Box, Button, Flex, Heading, Link, Text } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 const PatientFrontpage = () => {
   const navigate = useNavigate();
+  const { user } = useAuth();
 
   return (
     <Layout>
@@ -18,30 +20,32 @@ const PatientFrontpage = () => {
           w="full"
           maxW={{ base: "100%" }}
           textAlign="center"
-          mt={{ base: 3 }}
+          mt={{ base: 3, sm: 5 }}
         >
-          <Heading size="heading1">Velkommen Patient </Heading>
-          <Text size="body">Vælg en af nedenstående</Text>
+          <Heading size="heading1">
+            Velkommen, {user?.name || "Patient"}
+          </Heading>
+          <Text size="body" mt={{ base: 1, md: 2 }}>
+            Vælg en af nedenstående
+          </Text>
           <Flex
             direction="column"
             alignItems="center"
             maxW={{ base: "100%" }}
-            gap={{ base: 3 }}
-            mt={{ base: 5 }}
+            gap={{ base: 3, lg: 4 }}
+            mt={{ base: 5, sm: 6 }}
           >
             <Button
-              as={Link}
               onClick={() => navigate("/patient/ai")}
               variant="solidRed"
-              w={{ base: "15rem", sm: "17rem", md: "18rem", xl: "20rem" }}
+              w={{ base: "17rem", sm: "17rem", md: "18rem", xl: "20rem" }}
             >
               Snak med Klinika Assistent
             </Button>
             <Button
-              as={Link}
               onClick={() => navigate("/patient/appointments")}
               variant="solidRed"
-              w={{ base: "15rem", sm: "17rem", md: "18rem", xl: "20rem" }}
+              w={{ base: "17rem", sm: "17rem", md: "18rem", xl: "20rem" }}
             >
               Gå til Min Side
             </Button>
