576647a3e55614fe8e1d2c770bb22577e369a66c
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-06-05 13:28:05 +0200

    fix: Fix Wrap user in an object with user as key to get user's information

--- backend/src/controllers/fundament/user.controller.ts
+++ backend/src/controllers/fundament/user.controller.ts
@@ -13,7 +13,7 @@ export const getMyProfile = async (req: Request, res: Response) => {
       return;
     }
 
-    res.status(200).json(user);
+    res.status(200).json({ user });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
