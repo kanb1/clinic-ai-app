5baf0ca7cac8cfce7d1ccdee00b22e88a1d38861
Kanza Bokhari <58469168+kanb1@users.noreply.github.com>
2025-06-12 12:28:20 +0200

    fix: refetch userprofile after update

--- frontend/src/hooks/patient/settingHooks/useUpdateMyProfile.ts
+++ frontend/src/hooks/patient/settingHooks/useUpdateMyProfile.ts
@@ -1,11 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { api } from "@/services/httpClient";
 
 export const useUpdateMyProfile = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: async (formData: { email?: string; phone?: string }) => {
       const res = await api.put("/patients/profile", formData);
       return res.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["myProfile"] });
     },
   });
 };
